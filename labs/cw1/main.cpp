2f180bb933f39468e5755ef10fd1cdc0548834fb
Romeo Mcdonald <60313696+Ghoulbinder@users.noreply.github.com>
2024-10-08 11:40:35 +0100

    created cw1.cu
    started the cw1 created the .cu file for it

--- labs/cw1/main.cpp
+++ labs/cw1/main.cpp
@@ -71,7 +71,7 @@ int calc_token_occurrences(const std::vector<char>& data, const char* token)
 int main()
 {
     // Example chosen file
-    const char * filepath = "dataset/shakespeare.txt";
+    const char * filepath = "C:/set10108-cw/set10108/labs/cw1/dataset/shakespeare.txt";
 
     std::vector<char> file_data = read_file(filepath);
     if (file_data.empty())
